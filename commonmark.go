73f701b9b3e2c9959fac90d3fa77c2de8e9c7303
James Adam <james@jamesadam.me>
2014-11-22 17:22:16 -0500

    More doc updates

--- commonmark.go
+++ commonmark.go
@@ -1,13 +1,16 @@
+//Package commonmark provides a Go wrapper for CommonMark C Library
 package commonmark
 
-// #cgo LDFLAGS: -lcmark
-// #include "cmark.h"
+/*
+#cgo LDFLAGS: -lcmark
+#include "cmark.h"
+*/
 import "C"
 import "strings"
 
-//Converts Markdo--, er, CommonMark text to Html
-//mdtext -- input CommonMark text
-//returns HTML string
+// Converts Markdo--, er, CommonMark text to Html.
+// Parameter mdtext contains CommonMark text.
+// The return value is the HTML string
 func Md2Html(mdtext string) string {
 	//The call to cmark will barf if the input string doesn't end with a newline
 	if !strings.HasSuffix(mdtext, "\n") {
