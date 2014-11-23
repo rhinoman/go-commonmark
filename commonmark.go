e5cf91fd9d2a858be1844924451f796521402a9e
James Adam <james@jamesadam.me>
2014-11-22 20:24:23 -0500

    Memory Management :)

--- commonmark.go
+++ commonmark.go
@@ -3,10 +3,12 @@ package commonmark
 
 /*
 #cgo LDFLAGS: -lcmark
+#include <stdlib.h>
 #include "cmark.h"
 */
 import "C"
 import "strings"
+import "unsafe"
 
 // Converts Markdo--, er, CommonMark text to Html.
 // Parameter mdtext contains CommonMark text.
@@ -18,6 +20,59 @@ func Md2Html(mdtext string) string {
 	}
 	mdCstr := C.CString(mdtext)
 	strLen := C.int(len(mdtext))
+	defer C.free(unsafe.Pointer(mdCstr))
 	htmlString := C.cmark_markdown_to_html(mdCstr, strLen)
+	defer C.free(unsafe.Pointer(htmlString))
 	return C.GoString(htmlString)
+}
+
+type CMarkParser struct {
+	parser *C.struct_cmark_doc_parser
+}
+
+type CMarkDocument struct {
+	document *C.struct_cmark_node
+}
+
+// Retruns a new CMark Parser.
+// You must call Free() on this thing when you're done with it!
+// Please.
+func NewCmarkDocParser() *CMarkParser {
+	return &CMarkParser{
+		parser: C.cmark_new_doc_parser(),
+	}
+}
+
+// Process a line
+func (cmp *CMarkParser) ProcessLine(buffer string) {
+	s := len(buffer)
+	C.process_line(cmp.parser, C.CString(buffer), C.size_t(s))
+}
+
+// Finish parsing and generate a document
+// You must call Free() on the document when you're done with it!
+func (cmp *CMarkParser) Finish() *CMarkDocument {
+	return &CMarkDocument{
+		document: C.cmark_finish(cmp.parser),
+	}
+}
+
+// Cleanup the parser
+// Once you call Free on this, you can't use it anymore
+func (cmp *CMarkParser) Free() {
+	C.cmark_free_doc_parser(cmp.parser)
+}
+
+// Renders the document as HTML.
+// Retunrs an HTML string.
+func (doc *CMarkDocument) RenderHtml() string {
+	result := C.cmark_render_html(doc.document)
+	defer C.free(unsafe.Pointer(result))
+	return C.GoString(result)
+}
+
+// Cleanup the document
+// Once you call Free on this, you can't use it anymore
+func (doc *CMarkDocument) Free() {
+	C.cmark_free_nodes(doc.document)
 }
--- commonmark_test.go
+++ commonmark_test.go
@@ -12,3 +12,23 @@ func TestMd2Html(t *testing.T) {
 	}
 	t.Logf("Html Text: %v", htmlText)
 }
+
+func TestCMarkParser(t *testing.T) {
+	parser := commonmark.NewCmarkDocParser()
+	if parser == nil {
+		t.Error("Parser is nil!")
+	}
+	parser.ProcessLine("Boo\n")
+	parser.ProcessLine("===\n")
+	document := parser.Finish()
+	if document == nil {
+		t.Error("Document is nil!")
+	}
+	parser.Free()
+	htmlText := document.RenderHtml()
+	if htmlText != "<h1>Boo</h1>\n" {
+		t.Error("Html text is not as expected :(")
+	}
+	t.Logf("Html Text: %v", htmlText)
+	document.Free()
+}
